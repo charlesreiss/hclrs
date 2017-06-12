ef50edd6ad9ff4a7af71a052d8fd281fac0f603a
Charles  Reiss <cr4bd@virginia.edu>
2017-06-12 00:27:27 -0400

    Fuller expession support; refactor wire width representation

--- src/ast.rs
+++ src/ast.rs
@@ -1,15 +1,95 @@
 extern crate extprim;
 
-pub type WireWidth = usize;
-pub type WireValue = extprim::u128::u128;
+use extprim::u128::u128;
 
-#[derive(Debug)]
+use std::str::FromStr;
+use std::num::ParseIntError;
+use std::cmp;
+
+#[derive(Clone,Debug,Eq,PartialEq)]
+pub enum WireWidth {
+    Bits(usize),
+    Unlimited,
+}
+
+impl From<usize> for WireWidth {
+    fn from(s: usize) -> Self { WireWidth::Bits(s) }
+}
+
+impl FromStr for WireWidth {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match usize::from_str(s) {
+            Ok(s) => Ok(WireWidth::Bits(s)),
+            Err(x) => Err(x)
+        }
+    }
+}
+
+impl WireWidth {
+    fn bits_or(&self, s: usize) -> usize {
+        match self {
+            &WireWidth::Bits(t) => t,
+            &WireWidth::Unlimited => s,
+        }
+    }
+        
+    fn min(&self, w: WireWidth) -> WireWidth {
+        match self {
+            &WireWidth::Unlimited => w,
+            &WireWidth::Bits(s) => WireWidth::Bits(cmp::min(s, w.bits_or(s)))
+        }
+    }
+        
+    fn max(&self, w: WireWidth) -> WireWidth {
+        match self {
+            &WireWidth::Unlimited => w,
+            &WireWidth::Bits(s) => WireWidth::Bits(cmp::max(s, w.bits_or(s)))
+        }
+    }
+}
+
+// FIXME: disallow Eq?
+#[derive(Clone,Eq,PartialEq,Debug)]
+pub struct WireValue {
+    pub bits: u128,
+    pub width: WireWidth
+}
+
+impl WireValue {
+    pub fn new(v: u128) -> WireValue {
+        WireValue { bits: v, width: WireWidth::Unlimited }
+    }
+
+    pub fn from_binary(s: &str) -> WireValue {
+        WireValue {
+            bits: u128::from_str_radix(s, 2).unwrap(),
+            width: WireWidth::Bits(s.len()),
+        }
+    }
+
+    pub fn from_decimal(s: &str) -> WireValue {
+        WireValue {
+            bits: u128::from_str_radix(s, 10).unwrap(),
+            width: WireWidth::Unlimited,
+        }
+    }
+    
+    pub fn from_hexadecimal(s: &str) -> WireValue {
+        WireValue {
+            bits: u128::from_str_radix(s, 16).unwrap(),
+            width: WireWidth::Unlimited,
+        }
+    }
+}
+
+#[derive(Debug,Eq,PartialEq)]
 pub struct WireDecl {
     pub name: String,
     pub width: WireWidth,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Eq,PartialEq)]
 pub enum BinOpCode {
     Add,
     Sub,
@@ -24,26 +104,28 @@ pub enum BinOpCode {
     GreaterEqual,
     Less,
     Greater,
+    LogicalAnd,
+    LogicalOr,
+    LeftShift,
+    RightShift,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Eq,PartialEq)]
 pub enum UnOpCode {
     Negate,
     Complement,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Eq,PartialEq)]
 pub struct MuxOption {
     condition: Box<Expr>,
     value: Box<Expr>,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Eq,PartialEq)]
 pub enum Expr {
-    Constant(WireValue, WireWidth),
+    Constant(WireValue),
     BinOp(BinOpCode, Box<Expr>, Box<Expr>),
     UnOp(UnOpCode, Box<Expr>),
     Mux(Vec<MuxOption>),
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate lalrpop_util;
+#[cfg(test)]
+extern crate extprim;
 
 pub mod parser;
 mod ast;
 
 use parser::{parse_Expr, parse_WireDecls};
+#[cfg(test)]
+use ast::{Expr, WireDecl, WireValue, WireWidth, BinOpCode, UnOpCode};
 
 fn main() {
     let mut errors = Vec::new();
@@ -17,6 +21,79 @@ fn main() {
     );
     println!(
         "{:?}",
-        parse_Expr(&mut errors, "0b1000 * 15 + 0x1234 / 5 > 10").unwrap()
+        parse_Expr(&mut errors, "0b1000 * 15").unwrap()
     );
+    println!(
+        "{:?}",
+        parse_Expr(&mut errors, "0b1000 * 15 + 1").unwrap()
+    );
+    println!(
+        "{:?}",
+        parse_Expr(&mut errors, "0b1000 * 15 + 1 > 0").unwrap()
+    );
+    println!(
+        "{:?}",
+        parse_Expr(&mut errors, "0b1000 & (15 + 1) > 5 && 0x1234 < 3 || 4 >= 1 << 1 / 5").unwrap()
+    );
+}
+
+#[test]
+fn test_binops() {
+    let mut errors = Vec::new();
+    assert_eq!(
+        parse_Expr(&mut errors, "0b1000 * 15").unwrap(),
+        Box::new(Expr::BinOp(BinOpCode::Mul,
+                    Box::new(Expr::Constant(WireValue::from_binary("1000"))),
+                    Box::new(Expr::Constant(WireValue::from_decimal("15")))),
+        )
+    );
+    assert_eq!(
+        parse_Expr(&mut errors, "0b1000 * 15 + 1").unwrap(),
+        Box::new(Expr::BinOp(BinOpCode::Add,
+            Box::new(Expr::BinOp(BinOpCode::Mul,
+                                 Box::new(Expr::Constant(WireValue::from_binary("1000"))),
+                                 Box::new(Expr::Constant(WireValue::from_decimal("15"))),
+                                )),
+            Box::new(Expr::Constant(WireValue::from_decimal("1"))),
+        ))
+    );
+    assert_eq!(
+        parse_Expr(&mut errors, "0b1000 + 15 * 1").unwrap(),
+        Box::new(Expr::BinOp(BinOpCode::Add,
+            Box::new(Expr::Constant(WireValue::from_binary("1000"))),
+            Box::new(Expr::BinOp(BinOpCode::Mul,
+                                 Box::new(Expr::Constant(WireValue::from_decimal("15"))),
+                                 Box::new(Expr::Constant(WireValue::from_decimal("1"))),
+                                )),
+        ))
+    );
+    assert_eq!(
+        parse_Expr(&mut errors, "0b1000 * 15 + 1 > 0").unwrap(),
+        Box::new(Expr::BinOp(BinOpCode::Greater,
+            parse_Expr(&mut errors, "0b1000 * 15 + 1").unwrap(),
+            parse_Expr(&mut errors, "0").unwrap(),
+        ))
+    );
+    assert_eq!(
+        parse_Expr(&mut errors, "0b1000 & (15 + 1) > 5 && 0x1234 < 3 || 4 >= 1 << 1 / 5").unwrap(),
+        parse_Expr(&mut errors, "((0b1000 & (15 + 1)) > 5) && (0x1234 < 3) || (4 >= (1 << (1 / 5)))").unwrap()
+    );
+}
+
+#[test]
+fn test_wiredecls() {
+    let mut errors = Vec::new();
+    assert_eq!(
+        parse_WireDecls(&mut errors, "wire x : 32 , y : 2, z : 1;").unwrap(),
+        vec!(WireDecl { name: String::from("x"), width: WireWidth::Bits(32) },
+             WireDecl { name: String::from("y"), width: WireWidth::Bits(2) },
+             WireDecl { name: String::from("z"), width: WireWidth::Bits(1) })
+    );
+    assert_eq!(errors, vec!());
+    errors.clear();
+    assert_eq!(
+        parse_WireDecls(&mut errors, "wire x : 64;").unwrap(),
+        vec!(WireDecl { name: String::from("x"), width: WireWidth::Bits(64) })
+    );
+    assert_eq!(errors, vec!());
 }
