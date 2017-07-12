9f8e2d97361540b3d1ef04a7864be4c143c6713c
Charles Reiss <cr4bd@virginia.edu>
2017-07-11 20:08:09 -0400

    add --check option; remove extra newline from output

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,27 @@ fn main() {
     main_real().unwrap();
 }
 
-fn run_y86<W1: Write, W2: Write, W3: Write>(file_contents: &FileContents, yo_path: &Path,
+fn parse_y86(file_contents: &FileContents) -> Result<RunningProgram, Error> {
+    let program = parse_y86_hcl(file_contents)?;
+    let running_program = RunningProgram::new_y86(program);
+    Ok(running_program)
+}
+
+fn run_y86<W1: Write, W2: Write, W3: Write>(mut running_program: RunningProgram, yo_path: &Path,
            trace_out: &mut W1, step_out: &mut W2,
            disasm_out: &mut W3,  dump_registers: bool, timeout: u32) -> Result<(), Error> {
-    let program = parse_y86_hcl(file_contents)?;
-    let mut running_program = RunningProgram::new_y86(program);
     let mut yo_reader = BufReader::new(File::open(yo_path)?);
     running_program.set_timeout(timeout);
     running_program.load_memory_y86(&mut yo_reader)?;
     running_program.run_with_trace(step_out, trace_out, disasm_out, dump_registers)?;
-    println!("{}", running_program.dump_y86_str(dump_registers));
+    print!("{}", running_program.dump_y86_str(dump_registers));
     Ok(())
 }
 
 fn print_usage(program_name: &str, opts: Options) { 
-    let header = format!("Usage: {} [options] HCL-FILE YO-FILE [TIMEOUT]\nDefault timeout is 9999 cycles.",
+    let header = format!("Usage: {} [options] HCL-FILE [YO-FILE [TIMEOUT]]\n\
+                          Runs HCL_FILE on YO-FILE. If --check is specified, no YO-FILE may be supplied.\n\
+                          Default timeout is 9999 cycles.",
         program_name);
     print!("{}", opts.usage(&header));
 }
@@ -39,6 +45,7 @@ fn main_real() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     let program_name = args[0].clone();
     let mut opts = Options::new();
+    opts.optflag("c", "check", "check syntax only");
     opts.optflag("d", "debug", "output traces of all assignments for debugging");
     opts.optflag("q", "quiet", "only output state at the end");
     opts.optflag("t", "testing", "do not output custom register banks (for autograding)");
@@ -55,18 +62,43 @@ fn main_real() -> Result<(), Error> {
     let mut disasm_out: Box<Write> = if parsed_opts.opt_present("q") { Box::new(sink()) } else { Box::new(stdout()) };
     let mut trace_out: Box<Write> = if parsed_opts.opt_present("d") { Box::new(stdout()) } else { Box::new(sink()) };
     let dump_registers = !parsed_opts.opt_present("t");
+    let check_only = parsed_opts.opt_present("c");
     let free_args = parsed_opts.free;
-    if free_args.len() < 2 || free_args.len() > 3 {
+    if free_args.len() < 1 {
         print_usage(&program_name, opts);
         return Ok(());
     }
     let filename: &str = &free_args[0];
+    let path = Path::new(filename);
+    let file_contents = read_y86_hcl(path)?;
+    if free_args.len() > 3 {
+        print_usage(&program_name, opts);
+        return Ok(());
+    }
+    let running_program =
+        match parse_y86(&file_contents) {
+            Err(e) => {
+                e.format_for_contents(&mut stderr(), &file_contents)?;
+                return Ok(());
+            },
+            Ok(p) => p,
+        };
+    if check_only {
+        return Ok(());
+    }
+    if free_args.len() < 2 || free_args.len() > 3 {
+        print_usage(&program_name, opts);
+        return Ok(());
+    }
     let yo_filename: &str = &free_args[1];
     let timeout = 
         if free_args.len() > 2 {
             match u32::from_str_radix(&free_args[2], 10) {
                 Ok(x) => x,
-                Err(_) => panic!("timeout {} is not a number", &free_args[2]),
+                Err(_) => {
+                    print!("timeout {} is not a number", &free_args[2]);
+                    return Ok(());
+                }
             }
         } else {
             9999
@@ -74,7 +106,7 @@ fn main_real() -> Result<(), Error> {
     let path = Path::new(filename);
     let file_contents = read_y86_hcl(path)?;
     let yo_path = Path::new(yo_filename);
-    match run_y86(&file_contents, yo_path, &mut trace_out, &mut step_out, &mut disasm_out, dump_registers, timeout) {
+    match run_y86(running_program, yo_path, &mut trace_out, &mut step_out, &mut disasm_out, dump_registers, timeout) {
         Err(e) => e.format_for_contents(&mut stderr(), &file_contents)?,
         _ => {},
     }
