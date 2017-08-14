c8fe59809bf55b81d43af2fc7a798ef4497b806a
Charles  Reiss <cr4bd@virginia.edu>
2017-08-14 18:20:31 -0400

    generate spammy LALRPOP parser

--- build.rs
+++ build.rs
@@ -4,5 +4,5 @@ use std::env;
 fn main() {
     // FIXME: workaround for lalrpop issue #240; should eventually be removed.
     env::set_var("LALRPOP_LANE_TABLE", "disabled");
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new().emit_comments(true).emit_report(true).process_current_dir().unwrap();
 }
