31d65a76948bcfc06340dc072ab8d65a5c0f250e
Charles  Reiss <cr4bd@virginia.edu>
2018-02-01 08:35:42 -0500

    remove workaround for old lalrpop issue

--- build.rs
+++ build.rs
@@ -2,7 +2,5 @@ extern crate lalrpop;
 use std::env;
 
 fn main() {
-    // FIXME: workaround for lalrpop issue #240; should eventually be removed.
-    env::set_var("LALRPOP_LANE_TABLE", "disabled");
     lalrpop::Configuration::new().emit_comments(true).emit_report(true).process_current_dir().unwrap();
 }
